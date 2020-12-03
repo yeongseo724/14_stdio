c3e6b9ce938f47ab4946dc76bae646e51ff328b3
yeongseo724 <70522995+yeongseo724@users.noreply.github.com>
2020-12-03 17:01:27 +0900

    14 coding(4)

--- main.c
+++ main.c
@@ -1,40 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "linkedlist.h"
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-struct Book{
-	int number;
-	char title[20];
-};
-
 int main(int argc, char *argv[]) {
-	int i;
-	struct Book **bookshelf;
+	int n[5];
 	
-	bookshelf= (struct Book**)malloc(3*sizeof(struct Book*));
-	for(i=0;i<3;i++){
-		bookshelf[i]= (struct Book*)malloc(10*sizeof(struct Book));
-	}
+	printf("input 5 integers :");
+	scanf("%d %d %d %d %d", n, n+1, n+2, n+3, n+4);
+
+	insertDataToTail(n[0]);
+	insertDataToTail(n[1]);
+	insertDataToTail(n[2]);
+	insertDataToTail(n[3]);
+	insertDataToTail(n[4]);
 	
-	/*
-	if(p==NULL){
-		printf("memory allocation error!\n");
-		return -1;
-	}*/
-	
-	bookshelf[1][3].number =5;
-	strcpy(bookshelf[1][3].title, "C++ Programming");
-	
-	(bookshelf[2]+4)->number =3;
-	strcpy((bookshelf[2]+4)->title, "Communications Theory");
-	
-	printf("book (1,3) : %i, %s\n", (bookshelf[1]+3)->number, (bookshelf[1]+3)->title);
-	printf("book (2,4) : %i, %s\n", bookshelf[2][4].number, bookshelf[2][4].title);
-		
-	for(i=0;i<3;i++)
-		free(bookshelf[i]);
-	free(bookshelf);	
+	print_list();
+	print_node(2);
 	
 	return 0;
 }
